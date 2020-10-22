7a635a37b6c8297130aa0749c77af251e8307850
p.venhryn <p.venhryn@pozitiff.ua>
2020-10-23 01:42:07 +0300

    sadsad

--- static/js/common.js
+++ static/js/common.js
@@ -24,3 +24,27 @@ function onFormSubmit(){
     j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
     f.parentNode.insertBefore(j, f);
   })(window, document, "script", "dataLayer", "GTM-M3MWJWX");
+
+  var lazyImages = [].slice.call(document.querySelectorAll(".lazy"));
+  
+  if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+              let lazyImage = entry.target;
+              console.log(lazyImage);
+              lazyImage.src = lazyImage.dataset.img;
+              lazyImage.style = 'background-image: url(' + lazyImage.src + ')';
+              lazyImage.classList.remove("lazy");
+              lazyImageObserver.unobserve(lazyImage);
+            }
+        });
+      });
+
+      lazyImages.forEach(function(lazyImage) {
+          lazyImageObserver.observe(lazyImage);
+      });
+    } else {
+        // Possibly fall back to event handlers here
+        console.log('img false')
+    }
